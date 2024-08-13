2ecce02065303aed7191332b943f3e2fa72e4bfe
yonghyuny <lovelove4695@naver.com>
2024-08-13 16:50:00 +0900

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Customer = () => {
+  return <p>Customer Page</p>;
+};
+
+export default Customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Invoices = () => {
+  return <p>Invoice Page</p>;
+};
+
+export default Invoices;
